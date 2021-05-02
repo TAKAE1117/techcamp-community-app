86a4903b24c405117f1b10333d6656fde383f4ca
TAKAE1117 <takayuki.ogawa.0712@gmail.com>
2021-05-02 18:11:18 +0900

    Article model unit test code

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :article do
-    
+    title {Faker::Lorem.word}
+    genre_id {Faker::Number.between(from: 1, to: 10)}
+    text {Faker::Lorem.sentence}
+    association :user 
   end
 end
